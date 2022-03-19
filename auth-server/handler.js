e7da5bfad6aeeb72b68bf62bb6e025175bd8ea36
norihess <Norihess19@gmail.com>
2022-03-19 09:31:39 -0600

    updating

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -20,7 +20,7 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://norihess.github.io/meetup"],
-  javascript_origins: ["https://norihess.github.io", "http://localhost:3000"],
+  javascript_origins: ["https://norihess.github.io", "http://localhost:3000", "http://127.0.0.1:5500", "http://localhost:5500"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(

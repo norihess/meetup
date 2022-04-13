7775f3070158afa34e9f21879923b28305171672
norihess <Norihess19@gmail.com>
2022-04-13 11:38:44 -0600

    updated

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+	state = {
+		query: '',
+	}
+	handleInputChanged = (event) => {
+		const value = event.target.value;
+		this.setState({ query: value });
+	}
+	render() {
+		return (
+			<div className="CitySearch">
+				<input
+					type="text"
+					className="city"
+					value={this.state.query}
+					onChange={this.handleInputChanged}
+				/>
+				<ul className = "suggestions">
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default CitySearch;

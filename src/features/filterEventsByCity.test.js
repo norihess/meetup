c60043161624d56720c615d3deba85026527ac9a
norihess <Norihess19@gmail.com>
2022-05-16 08:51:54 -0600

    added scenario 2

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,8 +1,9 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import App from '../App';
 import { mockData } from '../mock-data';
+import CitySearch from '../CitySearch';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
@@ -24,16 +25,18 @@ defineFeature(feature, test => {
   });
 
   test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
+    let locations, CitySearchWrapper, suggestions;
     given('the main page is open', () => {
-
+			// CitySearchWrapper = shallow(<CitySearch locations={locations} updateEvents={() => {}} />);
+      CitySearchWrapper = shallow(<CitySearch updateEvents={() => {}} locations={locations} />);
     });
 
     when('the user starts typing in the city textbox', () => {
-
+      CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
     });
 
     then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
-
+      expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(2);
     });
   });
 

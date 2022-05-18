c60c6500dbf07b0fef0967b81abb7e1ee8dca7f1
norihess <Norihess19@gmail.com>
2022-05-17 19:24:52 -0600

    added scenario 1

--- src/test/EndToEnd.test.js
+++ src/test/EndToEnd.test.js
@@ -1,4 +1,22 @@
+import { puppeteerErrors } from "puppeteer";
+import puppeteer from 'puppeteer';
+
 describe (' show/hide an event details', () => {
 
-	
-})
+  test('User can expand an event to see its details', async () => {
+		beforeAll(async () => {
+			jest.setTimeout(30000);
+	});
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+
+    await page.waitForSelector('.event');
+    await page.click('.event .details-btn');
+
+    const eventDetails = await page.$('.event .event__Details');
+    expect(eventDetails).toBeDefined();
+    browser.close();
+  });
+
+});

aab8407b1a9f3e988122470795a555e498abc95d
norihess <Norihess19@gmail.com>
2022-05-17 19:36:59 -0600

    scenario 2 almost done

--- src/test/EndToEnd.test.js
+++ src/test/EndToEnd.test.js
@@ -2,11 +2,22 @@ import { puppeteerErrors } from "puppeteer";
 import puppeteer from 'puppeteer';
 
 describe (' show/hide an event details', () => {
+	let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+		jest.setTimeout(40000);
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
 
   test('User can expand an event to see its details', async () => {
-		beforeAll(async () => {
-			jest.setTimeout(30000);
-	});
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://localhost:3000/');
@@ -15,9 +26,21 @@ describe (' show/hide an event details', () => {
     await page.click('.event .details-btn');
 
     // const eventDetails = await page.$('.event .event__Details');
-		const eventDetails = await page.$('.eventList .event__Details');
+		const eventDetails = await page.$('.EventList .event__Details');
     expect(eventDetails).toBeDefined();
     browser.close();
   });
 
+	test('User can expand an event to see its details', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+
+    await page.waitForSelector('.EventList');
+    await page.click('.EventList .details-btn');
+
+    const eventDetails = await page.$('.EventList .event__Details');
+    expect(eventDetails).toBeDefined();
+    browser.close();
+  });
 });

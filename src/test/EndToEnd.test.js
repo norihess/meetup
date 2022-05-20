74949d26b34f9bd0571815e4248785344edc4ded
norihess <Norihess19@gmail.com>
2022-05-20 10:30:58 -0600

    update

--- src/test/EndToEnd.test.js
+++ src/test/EndToEnd.test.js
@@ -5,12 +5,8 @@ describe('show/hide an event details', () => {
   let page;
   beforeAll(async () => {
     jest.setTimeout(30000);
-		 // browser = await puppeteer.launch()
-      browser = await puppeteer.launch({
-		 	headless: false,
-		 	slowMo: 250, // slow down by 250ms
-		  ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
-		 });
+		browser = await puppeteer.launch()
+     
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');

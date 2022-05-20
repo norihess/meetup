fd2f1826a8b640edac50f0feffd14d43fbcc4b15
norihess <Norihess19@gmail.com>
2022-05-19 22:27:28 -0600

    created features file for showHideAnEventDetails and specifyNumberOfEvents with test.js

---
+++ src/features/showHideAnEventsDetails.test.js
@@ -0,0 +1,59 @@
+import { loadFeature, defineFeature } from 'jest-cucumber';
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import App from '../App';
+
+
+const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
+
+defineFeature(feature, test => {
+	let AppWrapper;
+
+	test('When an event element is collapsed by default', ({ given, when, then }) => {
+    given('the user is on the main page of the app', () => {
+			AppWrapper = mount(<App />);
+    });
+
+    when('an event is displayed', () => {
+      AppWrapper = mount(<App />);
+    });
+
+    then('the event details will be collapsed', () => {
+     expect(AppWrapper.find('.extra-details')).toHaveLength(0)
+    });
+  });
+
+  test('User can expand an event to see its details', ({ given, when, then }) => {
+		given('the user is displayed with a list of events', () => {
+      AppWrapper = await mount(<App />);
+    });  
+
+    when('the user clicks on an individual event', () => {
+      AppWrapper.update();
+      AppWrapper.find('.details-button').at(0).simulate('click');
+    });
+
+    then('the event details will be displayed', () => {
+      expect(AppWrapper.find('.extra-details')).toHaveLength(1);
+    });
+  });
+
+
+  test('User can collapse an event to hide its details', ({ given, and, when, then }) => {
+    given('The user has clicked on an event to display details', async () => {
+			AppWrapper = await mount(<App />);
+			AppWrapper.update();
+			AppWrapper.find('.details-button').at(0).simulate('click');
+			expect(AppWrapper.find('.extra-details')).toHaveLength(1);
+    });
+
+    when('the user clicks on “close” button', () => {
+      AppWrapper.find('.details-button').at(0).simulate('click');
+    });
+
+		then('the event details will hide', () => {
+      expect(AppWrapper.find('.extra-details')).toHaveLength(0);
+    });
+
+  });
+});
